5013131368a28a07a0213dc546e608a8475c12c2
NIta149 <84484456+NIta149@users.noreply.github.com>
2021-11-16 14:50:14 +0530

    Add files via upload

---
+++ python basic.py
@@ -0,0 +1,990 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+print ('Good Day')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# CTRL + ENTER  - Execute 
+# SHIFT +ENTER -  - Excute + Move to Next cell 
+# ALT +ENTER  - Execute + INsert Cell 
+
+# In[ ]:
+
+
+Good Day
+
+
+# In[ ]:
+
+
+Variable 
+
+
+# In[7]:
+
+
+a = 50
+print (a)
+
+
+# In[8]:
+
+
+print (type(a))
+
+
+# In[9]:
+
+
+a = 50.5
+print (a)
+print (type(a))
+
+
+# In[10]:
+
+
+a = 'Sunday'
+print (a)
+print (type(a))
+
+
+# In[11]:
+
+
+a= 50
+b =20
+c = a+b
+print (c)
+
+
+# In[14]:
+
+
+print (c)
+
+
+# In[15]:
+
+
+a = 'Sunday'
+b = 'Monday'
+print (a+b)
+
+
+# In[18]:
+
+
+print (a)
+print (b)
+
+
+# In[17]:
+
+
+print (b)
+
+
+# In[19]:
+
+
+print (a,b)
+
+
+# In[20]:
+
+
+print ('Today is ', a,' Tomorrow is ', b)
+
+
+# In[23]:
+
+
+print ('Today is Sunday Tomorrow is Monday')
+
+
+# In[24]:
+
+
+a = 10
+
+
+# In[25]:
+
+
+print (a)
+
+
+# In[26]:
+
+
+a = 50
+print (a)
+
+
+# In[27]:
+
+
+print (50)
+
+
+# In[28]:
+
+
+print (50)
+
+
+# In[29]:
+
+
+a10 = 100
+print (a10)
+
+
+# In[32]:
+
+
+a = 'Sunday'
+b = 'Monday'
+
+print ('Today is',a,'Tomorrow is', b)
+
+
+# In[35]:
+
+
+a = 10
+b = 50
+print (a,b)
+print(a)
+print (b)
+
+
+# In[ ]:
+
+
+1020
+
+
+# In[ ]:
+
+
+ab = 10
+
+
+# In[ ]:
+
+
+10 
+
+
+# In[ ]:
+
+
+10.5
+
+
+# In[36]:
+
+
+'Pune'
+
+
+# In[38]:
+
+
+a = 'Sunday'
+b = 'Monday' 
+print ('Today is',a,b)
+print(a)
+print (b)
+
+
+# In[ ]:
+
+
+'Today is'
+
+
+# In[39]:
+
+
+a = 'Sunday'
+b = 'Monday' 
+print ('Today is',a,'Tomorrow is',b)
+print(a)
+print (b)
+
+
+# In[ ]:
+
+
+a = 'Sunday'
+b = 'Monday' 
+print ('Today is',a,'Tomorrow is',b)
+print(a)
+print (b)
+
+
+# In[41]:
+
+
+print ('Sunday')
+
+
+# In[44]:
+
+
+'m'
+
+
+# In[ ]:
+
+
+a = 10
+b = 20
+print ('Today is',a,'Tomorrow is',b)
+print(a)
+print (b)
+
+
+# In[46]:
+
+
+a = "10"
+b = '20'
+print (a+b)
+
+
+# In[47]:
+
+
+a= "Today's"
+print (a)
+
+
+# In[49]:
+
+
+a= "Today's"
+print (a)
+
+
+# In[51]:
+
+
+a= 'Today"s'
+print (a)
+
+
+# In[52]:
+
+
+a = 10
+print (a)
+
+
+# In[53]:
+
+
+a = 10.5
+print (a)
+
+
+# In[54]:
+
+
+a = 'Today'
+print (a)
+
+
+# In[57]:
+
+
+a = 'Y'
+print (a)
+print (type(a))
+
+
+# In[58]:
+
+
+list1 = [10,20,30,40,50]
+list1
+
+
+# In[59]:
+
+
+list1[2]
+
+
+# In[68]:
+
+
+list1[0]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[60]:
+
+
+Emp= ['John', 25, 5000, 'TN']
+Emp
+
+
+# In[61]:
+
+
+Emp[1]
+
+
+# In[62]:
+
+
+Emp[3]
+
+
+# In[63]:
+
+
+print (Emp[1])
+
+
+# In[67]:
+
+
+print (a)
+print (b)
+print (c)
+
+
+# In[70]:
+
+
+c = 5+10j
+a = 10+5j
+print (c)
+print (type(c))
+
+
+# In[73]:
+
+
+c = 1+10j
+a = 10+5j
+print (c+a)
+print (type(c))
+
+
+# In[ ]:
+
+
+c = 5+10
+a = 10+5j
+print (c+a)
+print (type(c))
+
+
+# In[72]:
+
+
+c = 5+10
+c
+
+
+# In[75]:
+
+
+
+
+
+# In[76]:
+
+
+for i in range (5):
+    print(i)
+    
+
+
+# In[80]:
+
+
+for i in range (50,60):
+    print(i)
+
+
+# In[81]:
+
+
+for i in range (50,60,2):
+    print(i)
+
+
+# In[83]:
+
+
+for i in range (100,90,-1):
+    print(i)
+
+
+# In[84]:
+
+
+for i in range (100,50,-5):
+    print(i)
+
+
+# In[ ]:
+
+
+dict = {'Fist Name': 'John', 'Age': 25, 'Salary':5000}
+
+
+# In[ ]:
+
+
+Emp= ['John', 25, 5000, 'TN']
+Emp
+
+
+# In[85]:
+
+
+dict = {'Fist Name': 'John', 'Age': 25, 'Salary':5000}
+
+
+# In[86]:
+
+
+dict['Fist Name']
+
+
+# In[87]:
+
+
+dict['Age']
+
+
+# In[88]:
+
+
+l1 = [10,20,30,40,50,20,30,20]
+print (l1)
+
+
+# In[90]:
+
+
+l1[1]
+
+
+# In[89]:
+
+
+s1 = {10,20,30,40,50,20,30,20}
+print (s1)
+
+
+# In[91]:
+
+
+s[2]
+
+
+# list = []
+# tuple = ()
+# dict= {}
+# set = {}
+
+# In[92]:
+
+
+a = True
+print (a)
+
+
+# In[93]:
+
+
+print (type(a))
+
+
+# In[95]:
+
+
+a = 'TRUE'
+print (a)
+print (type(a))
+
+
+# In[96]:
+
+
+a = False
+print (a)
+print (type(a))
+
+
+# In[98]:
+
+
+a =False
+print (a)
+print (type(a))
+
+
+# In[99]:
+
+
+l1 = [10,20,30]
+print (l1)
+
+
+# In[101]:
+
+
+del(l1[1])
+print (l1)
+
+
+# In[103]:
+
+
+tup = (10,20,30)
+print (tup)
+print (type(tup))
+
+
+# In[104]:
+
+
+del(tup[1])
+
+
+# In[105]:
+
+
+list1 = [10,20,30]
+l1.append(50)
+print (l1)
+
+
+# In[108]:
+
+
+tup.append(50)
+
+
+# In[109]:
+
+
+a = 10
+print (a)
+
+
+# In[110]:
+
+
+print (float(a))
+
+
+# In[111]:
+
+
+a = 15.75
+print (a)
+
+
+# In[112]:
+
+
+print (int (a))
+
+
+# In[114]:
+
+
+c = str(a)
+print (c)
+
+
+# In[115]:
+
+
+print (type(c))
+
+
+# In[116]:
+
+
+a = True 
+print (int (a))
+
+
+# In[117]:
+
+
+a = False
+print (int (a))
+
+
+# In[118]:
+
+
+print (bool(0))
+
+
+# In[119]:
+
+
+print (bool(500))
+
+
+# In[120]:
+
+
+print (bool(-500))
+
+
+# In[123]:
+
+
+list1 = [10,20,30, 30,20,10]
+print (list1)
+
+
+# In[124]:
+
+
+print (tuple(list1))
+
+
+# In[125]:
+
+
+print (set(list1))
+
+
+# In[126]:
+
+
+dict = {'Age':[10,20,30.35] }
+
+
+# In[129]:
+
+
+dict['Age']
+
+
+# In[130]:
+
+
+dict['Age'].append (40)
+
+
+# In[131]:
+
+
+dict
+
+
+# In[132]:
+
+
+dict['Salary'] = 5000
+dict
+
+
+# In[134]:
+
+
+dict['Age'][3]
+
+
+# In[135]:
+
+
+del(dict['Age'][3])
+dict
+
+
+# In[136]:
+
+
+del(dict['Age'][3]
+
+
+# In[140]:
+
+
+a = [10,20,30,40,60,70]
+len(a)
+
+
+# In[145]:
+
+
+h = len(a)/2
+
+
+# In[146]:
+
+
+a[int(h)]
+
+
+# In[147]:
+
+
+a = [10,20,30,40,60,70]
+len(a)
+
+
+# In[ ]:
+
+
+a[int(h)]
+
+
+# In[148]:
+
+
+a = [10,20,30,40,60,70]
+len(a)
+
+
+# In[152]:
+
+
+(len(a)/2-1)
+
+
+# In[151]:
+
+
+a[13]
+
+
+# In[ ]:
+
+
+a = [10,20,30,40,60,70]
+len(a)
+
+
+# In[156]:
+
+
+a = [10,20,30,40,60]
+(len(a)//2 )+1
+
+
+# In[157]:
+
+
+a = [10,20,30,40,60,70]
+
+(len(a)//2 )+1
+
+
+# In[159]:
+
+
+a = [10,20,30,40,60]
+(len(a)//2 )+1
+
+
+# In[160]:
+
+
+l1 = [10,20,30]
+print (30 in l1)
+
+
+# In[170]:
+
+
+for i in l1:
+    if (i == 20):
+        print (i)
+        
+
+
+# In[5]:
+
+
+a=2.1
+b=type str()
+print(b)
+    
+
+
+# In[9]:
+
+
+a=list[]
+b=[1,2,3,4]
+c=b.append(a)
+print(c)
+
+
+# In[11]:
+
+
+userAge = [21, 22, 23, 24, 25]
+print(userAge[-1])
+
+
+# In[12]:
+
+
+userAge3 = userAge[2:4]
+print(userAge3)
+
+
+# In[15]:
+
+
+Age4 = userAge[1:5:2]
+print(Age4)
+
+
+# In[16]:
+
+
+print(userAge[1: ])
+
+
+# In[17]:
+
+
+print(userAge[ :4])
+
+
+# In[18]:
+
+
+userAge[1] = 5
+print(userAge)
+
+
+# In[19]:
+
+
+userAge.append(99)
+print(userAge)
+
+
+# In[20]:
+
+
+del userAge[2]
+print(userAge)
+
+
+# In[28]:
+
+
+a=str myList = [1, 2, 3, 4, 5,“Hello”]
+print(a)
+
+
+# In[29]:
+
+
+print(myList[2])
+
+
+# In[31]:
+
+
+monthsOfYear = (“Jan”
+("")
+“Feb”
+("")
+“Mar”
+("")
+“Apr”
+("")
+“May”
+
+print(monthsOfYear[0])
+
+
+# In[33]:
+
+
+myDict = {'One':1.35, 2.5:'Two Point Five', 3:''+'',7.9:2}
+print(myDict)
+
+
+# In[35]:
+
+
+print(myDict['One'])
+
+
+# In[36]:
+
+
+myName = input("Please enter your name: ")
+myAge = input("What about your age: ")
+print ("Hello World, my name is"
+, myName,
+"and I am"
+,
+myAge,
+"years old.")
+
+
+# In[2]:
+
+
+f = open ('myfile.txt','r')
+for line in f:
+print (line, end = ‘’)
+f.close()
+
+
+# In[7]:
+
+
+f = open ('myfile.txt','r')
+for line in f:
+    print (line, end = '')
+f.close()
+
+
+# In[ ]:
+
+
+
+
